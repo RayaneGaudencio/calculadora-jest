e9d84d83f88a164846befcbcf862ccaee2472c03
Rayane Gaudêncio <rayanegaudencio11@gmail.com>
2023-10-31 09:03:42 -0300

    first commit

---
+++ calculadora.test.js
@@ -0,0 +1,12 @@
+const { somar } = require('./calculadora')
+
+
+describe('Teste da Calculadora', () => {
+    test('deveria somar dois números', () => {
+      expect(somar(2, 3)).toBe(5)
+      expect(somar(-1, -1)).toBe(-2)
+      expect(somar(2.5, 2.25)).toBe(4.75)
+      expect(somar(10, 0)).toBe(10)
+    })
+    
+});
